eb3feee153d38d424acff583805a4f149b7836c1
David H. Angarita <davidangarita.sistemas@gmail.com>
2021-12-09 15:53:12 -0500

    katas 2,3,4,5,6,8,10 listos

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -4,10 +4,8 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
-
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /*
--- src/main/java/katas/Kata7.java
+++ src/main/java/katas/Kata7.java
@@ -20,11 +20,13 @@ public class Kata7 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return  movieLists.stream().flatMap(movie -> movie.getVideos().stream().map(video -> {
-            return ImmutableMap.of(
-                    "id",video.getId(),
-                    "title",video.getTitle(),
-                    "boxart",video.getBoxarts().stream().filter(el -> el.getWidth() > 150).findFirst().orElse(null).getUrl());
-        })).collect(Collectors.toList());
+        return movieLists.stream()
+                .flatMap(movie -> movie.getVideos()
+                        .stream().map(video -> {
+                            return ImmutableMap.of(
+                                    "id", video.getId(),
+                                    "title", video.getTitle(),
+                                    "boxart", video.getBoxarts().stream().filter(el -> el.getWidth() > 150).findFirst().orElse(null).getUrl());
+                        })).collect(Collectors.toList());
     }
 }
